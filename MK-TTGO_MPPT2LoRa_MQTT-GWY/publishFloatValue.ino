3a327f4917fc6bf292aafd8271c644243484a6e4
MK4001 <50479511+mk4001@users.noreply.github.com>
2024-01-26 11:19:19 +0100

    Update publishFloatValue.ino

--- MK-TTGO_MPPT2LoRa_MQTT-GWY/publishFloatValue.ino
+++ MK-TTGO_MPPT2LoRa_MQTT-GWY/publishFloatValue.ino
@@ -1,3 +1,9 @@
+/*
+  © MK 18 Jan 2024
+  LoRaReceiver MQTT Gateway
+  based on ESP32-TTGO sending packets on 868 MHz
+*/
+
 void publishFloatValue(const char* topic, float value) {
   char message[20];
   snprintf(message, sizeof(message), "%.2f", value);
