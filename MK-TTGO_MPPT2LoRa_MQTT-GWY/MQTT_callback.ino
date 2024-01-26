3f2687df163ad94d91f9e9b73005ad1f8e144be8
MK4001 <50479511+mk4001@users.noreply.github.com>
2024-01-26 11:18:37 +0100

    Update MQTT_callback.ino

--- MK-TTGO_MPPT2LoRa_MQTT-GWY/MQTT_callback.ino
+++ MK-TTGO_MPPT2LoRa_MQTT-GWY/MQTT_callback.ino
@@ -1,3 +1,9 @@
+/*
+  © MK 18 Jan 2024
+  LoRaReceiver MQTT Gateway
+  based on ESP32-TTGO sending packets on 868 MHz
+*/
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
